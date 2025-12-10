a178a408e2a215868fa9d841992a04482d0777e4
Muhammad Junaid Ahmad <pakjunaidahmad@gmail.com>
2025-12-10 13:27:22 +0500

    Refactor weather fetching logic and event listeners
    clean up script.js and improve code readability

--- weather app through js/script.js
+++ weather app through js/script.js
@@ -1,38 +1,3 @@
-// const apikey = "ccffe1fa1824317235e5988f34237a7a";
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=";
-// const units = "&units=metric";
-
-// const searchBox = document.querySelector("#inputfeild");
-// const searchBtn = document.querySelector(".searchbtn button");
-// console.log(searchBtn);
-// console.log(searchBox.value);
-// async function getWeather(city) {
-//   try {
-//     const response = await fetch(`${url}${city}${units}&appid=${apikey}`);
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.error("There has been a problem with your fetch operation:", error);
-//   }
-// document.querySelector(".weathercity h1").innerHTML = data.name;
-//   document.getElementsByClassName("weatherpoints").innerHTML = `${data.main.temp} °C`;
-//   document.getElementsByClassName( "humidity").innerHTML = `${data.main.humidity} %`;
-//   document.getElementsByClassName("wind").innerText = `${data.wind.speed} km/h`;
-// }
-// searchBtn.addEventListener("click", () => {
-//   getWeather(searchBox.value);
-// });
-// searchBox.addEventListener("keypress", (event) => {
-//   if (event.key === "Enter") {
-//     getWeather(searchBox.value);
-//   }
-// });
-
-
-
 const apikey = "ccffe1fa1824317235e5988f34237a7a";
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
 const units = "&units=metric";
@@ -71,3 +36,4 @@ searchBox.addEventListener("keypress", (event) => {
     getWeather(searchBox.value);
   }
 });
+
